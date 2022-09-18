c15da8b67362092e5426cefdcbf65082e241fb0e
Henry Oke <hoke@LMG-00053.local>
2022-09-18 18:23:41 -0400

    Checking favs page

--- pages/favorites.js
+++ pages/favorites.js
@@ -1,37 +1,37 @@
 import { getSession } from 'next-auth/react';
 import Layout from '@/components/Layout';
 import Grid from '@/components/Grid';
-import { prisma } from '@/lib/prisma';
+// import { prisma } from '@/lib/prisma';
 
-export async function getServerSideProps(context) {
-  // Check if user is authenticated
-  const session = await getSession(context);
+// export async function getServerSideProps(context) {
+//   // Check if user is authenticated
+//   const session = await getSession(context);
 
-  // If not, redirect to the homepage
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/',
-        permanent: false,
-      },
-    };
-  }
+//   // If not, redirect to the homepage
+//   if (!session) {
+//     return {
+//       redirect: {
+//         destination: '/',
+//         permanent: false,
+//       },
+//     };
+//   }
 
-  // Get all homes from the authenticated user
-  const homes = await prisma.home.findMany({
-    where: {
-      favoritedBy: { some: { email: session.user.email } },
-    },
-    orderBy: { createdAt: 'desc' },
-  });
+//   // Get all homes from the authenticated user
+//   const homes = await prisma.home.findMany({
+//     where: {
+//       favoritedBy: { some: { email: session.user.email } },
+//     },
+//     orderBy: { createdAt: 'desc' },
+//   });
 
-  // Pass the data to the Homes component
-  return {
-    props: {
-      homes: JSON.parse(JSON.stringify(homes)),
-    },
-  };
-}
+//   // Pass the data to the Homes component
+//   return {
+//     props: {
+//       homes: JSON.parse(JSON.stringify(homes)),
+//     },
+//   };
+// }
 
 const Favorites = ({ homes = [] }) => {
   return (
@@ -41,7 +41,7 @@ const Favorites = ({ homes = [] }) => {
         Manage your homes and update your listings
       </p>
       <div className="mt-8">
-        <Grid homes={homes} />
+        {/* <Grid homes={homes} /> */}
       </div>
     </Layout>
   );
